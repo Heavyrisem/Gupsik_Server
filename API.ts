d673dc9ddd164cd2d1926b62cfccb95de979d294
heavyrisem <pyo1748@gmail.com>
2021-10-25 15:18:39 +0900

    오류 처리

--- API.ts
+++ API.ts
@@ -25,13 +25,22 @@ interface APIResponse {
     }>
 }
 
+// const RecentMeal = {
+//     time: 
+// }
+
 export default {
     GetLunch: (time: string): Promise<string> => {
         return new Promise(async (resolve, reject) => {
-            let result: AxiosResponse<APIResponse> = await axios.post(`https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531328&MLSV_YMD=${time}&Type=json`);
-    
-            if (!result.data.mealServiceDietInfo.length) return reject("조회된 급식이 없습니다.");
-            else return resolve(result.data.mealServiceDietInfo[1].row[0].DDISH_NM.replace(/(\d+\.)*<br\/>/g, '. '));
+            try {
+                let result: AxiosResponse<APIResponse> = await axios.post(`https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531328&MLSV_YMD=${time}&Type=json`);
+        
+                if (!result.data.mealServiceDietInfo.length) return reject("조회된 급식이 없습니다.");
+                else return resolve(result.data.mealServiceDietInfo[1].row[0].DDISH_NM.replace(/(\d+\.)*<br\/>/g, '. '));
+            } catch (err) {
+                console.log(err);
+                return reject("급식 정보를 받아오는데 실패했습니다.");
+            }
         })
     }
 }
